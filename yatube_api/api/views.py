a4a46ef36a9d362d0b3307642fe439619fdbb9d1
Alexander Dondokov <adondokov@gmail.com>
2023-02-22 12:41:02 +0800

    edited postviewset

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,6 +3,7 @@ from posts.models import Comment, Group, Post, User, Follow
 from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+# from rest_framework.pagination import LimitOffsetPagination
 
 from .permissions import IsAuthorOrReadOnlyPermission
 from .serializers import (CommentSerializer,
@@ -23,6 +24,16 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
