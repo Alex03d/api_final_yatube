443dc1ebe05830336fdd1195766eb560bd2181e4
Alexander Dondokov <adondokov@gmail.com>
2023-02-23 00:17:24 +0800

    PEP8

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Comment, Group, Post, User, Follow
+from posts.models import Comment, Group, Post
 from rest_framework import viewsets, permissions, filters
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated, AllowAny
